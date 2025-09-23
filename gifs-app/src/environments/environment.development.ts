614d94ace97a3df94ab548e80f491e8012ae9adc
VICTHOR <ing.victhor1@gmail.com>
2025-09-23 17:27:46 -0600

    environments
    environments

---
+++ gifs-app/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    companyName: 'My Company',
+    companySlogan: 'My Company Slogan Development',
+    companyVersion: '1.0.0-dev'
+};
---
+++ gifs-app/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    companyName: 'My Company',
+    companySlogan: 'My Company Slogan',
+    companyVersion: '1.0.0'
+};
