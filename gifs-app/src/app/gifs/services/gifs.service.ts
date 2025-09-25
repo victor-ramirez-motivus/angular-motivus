0d0e2e935bf45ddbc14cad18ce644fc7424af243
VICTHOR <ing.victhor1@gmail.com>
2025-09-25 12:18:08 -0600

    Refactors trending GIFs display into a grid layout
    Improves the display of trending GIFs by organizing them into a responsive grid layout.
    This enhances the visual presentation and user experience on the trending page.
    
    The change introduces a computed signal to group the trending gifs into chunks, allowing to iterate over them to display the grid.

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -35,6 +35,16 @@ export class GifsService {
     trendingGifs = signal<Gif[]>([])
     trendingGifsLoading = signal(true)
 
+    trendingGifsGroup = computed<Gif[][]>(() => {
+        const groups = []
+
+        for (let i = 0; i < this.trendingGifs().length; i += 3) {
+            groups.push(this.trendingGifs().slice(i, i + 3));
+        }
+
+        return groups;
+    });
+
     searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
     searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
