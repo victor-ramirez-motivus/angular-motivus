3f77ea6306a3f4324a6799f9054d1f95e3ee9f37
VICTHOR <ing.victhor1@gmail.com>
2025-09-26 15:04:48 -0600

    Adds country layout with nested routing
    Implements a layout for the country feature module.
    
    This includes a parent component (`CountryLayoutComponent`) and sets up
    nested routing using `router-outlet`. A default route is configured to
    redirect to the 'by-capital' page.

--- country-app/src/app/country/country.routes.ts
+++ country-app/src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from "@angular/router";
+import { CountryLayoutComponent } from "./layouts/country-layout/country-layout.component";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
 
 export const countryRoutes: Routes = [
     {
         path: '',
-        component: ByCapitalPageComponent,
+        component: CountryLayoutComponent,
+        children: [
+            {
+                path: 'by-capital',
+                component: ByCapitalPageComponent
+            },
+            {
+                path: '**',
+                redirectTo: 'by-capital'
+            }
+        ]
     },
 ]
 
