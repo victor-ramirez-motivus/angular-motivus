1c15851c6a6b547ffa4507b0c77e37198fedc448
VICTHOR <ing.victhor1@gmail.com>
2025-09-29 17:49:44 -0600

    Configures application localization
    Configures the application to support multiple locales by registering locale data for Spanish and French.
    
    Provides dynamic locale switching based on the LocaleService.

--- pipes-app/src/app/app.config.ts
+++ pipes-app/src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { registerLocaleData } from '@angular/common';
 import { routes } from './app.routes';
+import { LocaleService } from './services/locale.service';
+
+import localeEs from '@angular/common/locales/es-MX';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      // useValue: 'fr',
+      deps: [LocaleService],
+      useFactory: (localeService: LocaleService) => localeService.getLocale,
+    },
+  ]
 };
