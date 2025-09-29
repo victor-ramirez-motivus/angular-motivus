5d83d31176033cc8fda102351c6542793d1f5f3d
VICTHOR <ing.victhor1@gmail.com>
2025-09-29 17:14:34 -0600

    Adds basic page to showcase Angular pipes
    Creates a basic page component to demonstrate the usage of built-in Angular pipes such as lowercase, uppercase, titlecase, and date pipes.
    
    Introduces a locale service to handle locale changes and persist the selected locale in local storage. The page includes buttons to change the locale to French, English, or Spanish. A ticking date updates every second, demonstrating the date pipe's reactivity.

--- pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
+  nameLower = signal('victor');
+  nameUpper = signal('VICTOR');
+  fullName = signal('vicTor RamiReZ');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
+
+  changeLocale(locale: AvailableLocale) {
+    console.log({ locale });
+    this.localeService.changeLocale(locale);
+  }
+}
