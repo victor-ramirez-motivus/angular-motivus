6eaafebd6e66348e3fd71075573a10dd8f3cdba5
VICTHOR <ing.victhor1@gmail.com>
2025-09-29 14:18:59 -0600

    Adds region filter functionality
    Implements the ability to filter countries by region.
    
    Introduces a region type and the by-region page, allowing users to select a region and view corresponding countries.
    
    Caches country data by region to improve performance.
    
    Also, makes capital optional for when countries don't have one.

--- country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
+++ country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,67 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject, linkedSignal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { Region } from '../../interfaces/region.type';
+import { CountryService } from '../../services/country.service';
+
+function validateQueryParam(queryParam: string): Region {
+  queryParam = queryParam.toLowerCase();
+
+  const validRegions: Record<string, Region> = {
+    africa: 'Africa',
+    americas: 'Americas',
+    asia: 'Asia',
+    europe: 'Europe',
+    oceania: 'Oceania',
+    antarctic: 'Antarctic',
+  };
+
+  return validRegions[queryParam] ?? 'Americas';
+}
 
 @Component({
   selector: 'app-by-region-page',
   imports: [CountryListComponent],
   templateUrl: './by-region-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ByRegionPageComponent { }
+export class ByRegionPageComponent {
+  countryService = inject(CountryService);
+  
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+    'Antarctic',
+  ];
+
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('region') ?? '';
+
+  selectedRegion = linkedSignal<Region>(() =>
+    validateQueryParam(this.queryParam)
+  );
+
+  countryResource = rxResource({
+    request: () => ({ region: this.selectedRegion() }),
+    loader: ({ request }) => {
+      console.log({ request: request.region });
+
+      if (!request.region) return of([]);
+
+      this.router.navigate(['/country/by-region'], {
+        queryParams: {
+          region: request.region,
+        },
+      });
+
+      return this.countryService.searchByRegion(request.region);
+    },
+  });
+
+}
--- country-app/src/app/country/services/country.service.ts
+++ country-app/src/app/country/services/country.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { catchError, delay, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
+import { Region } from '../interfaces/region.type';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { CountryMapper } from '../mapper/country.mapper';
 
@@ -14,11 +15,22 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
+  private queryCacheCapeital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
+  private queryCacheRegion = new Map<Region, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
+    query = query.toLowerCase();
+
+    if (this.queryCacheCapeital.has(query)) {
+      return of(this.queryCacheCapeital.get(query) ?? []);
+    }
+
+    console.log('Ejecutando consulta HTTP para capital ', query);
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap(countries => this.queryCacheCapeital.set(query, countries)),
       catchError((error) => {
         console.error('Error fetching ', error);
 
@@ -34,14 +46,43 @@ export class CountryService {
 
     query = query.toLowerCase();
 
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query) ?? []).pipe(
+        delay(2000)
+      );
+    }
+
+    console.log('Ejecutando consulta HTTP para país ', query);
+
     return this.http.get<RESTCountry[]>(url).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
       delay(2000),
+      tap(countries => this.queryCacheCountry.set(query, countries)),
       catchError((error) => {
         console.log('Error fetching ', error);
 
         return throwError(
           () => new Error(`No se pudo obtener países con ese query ${query}`)
+        );
+      })
+    );
+  }
+
+  searchByRegion(region: Region) {
+    const url = `${API_URL}/region/${region}`;
+
+    if (this.queryCacheCountry.has(region)) {
+      return of(this.queryCacheCountry.get(region) ?? []);
+    }
+
+    return this.http.get<RESTCountry[]>(url).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((countries) => this.queryCacheRegion.set(region, countries)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
+
+        return throwError(
+          () => new Error(`No se pudo obtener region con ${region}`)
         );
       })
     );
