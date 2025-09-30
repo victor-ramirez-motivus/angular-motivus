bffe99e5326324ca4cefdb8f869008bbd02e43b4
VICTHOR <ing.victhor1@gmail.com>
2025-09-30 14:58:21 -0600

    Configures routing and layout
    Sets up routing modules for reactive forms, auth, and country pages.
    
    Implements a basic application layout with a side menu.
    
    Removes default Angular template files.

---
+++ reactive-forms-app/src/app/reactive/auth.routes.ts
@@ -0,0 +1,32 @@
+import { Routes } from "@angular/router";
+import { BasicPageComponent } from "./pages/basic-page/basic-page.component";
+import { DynamicPageComponent } from "./pages/dynamic-page/dynamic-page.component";
+import { SwitchesPageComponent } from "./pages/switches-page/switches-page.component";
+
+
+export const reactiveRoutes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: 'basic',
+                title: 'Basicos',
+                component: BasicPageComponent
+            },
+            {
+                path: 'dynamic',
+                title: 'Dinamicos',
+                component: DynamicPageComponent
+            },
+            {
+                path: 'switches',
+                title: 'Switches',
+                component: SwitchesPageComponent
+            },
+            {
+                path: '**', 
+                redirectTo: 'basic'
+            }
+        ]
+    }
+]
---
+++ reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,10 @@
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [JsonPipe],
+  templateUrl: './basic-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BasicPageComponent { }
