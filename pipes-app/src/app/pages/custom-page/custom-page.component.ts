db51203714bb5fd616c3abd9dda238b74c43be3d
VICTHOR <ing.victhor1@gmail.com>
2025-09-30 13:56:09 -0600

    Adds custom pipes and a custom page
    Implements a custom page to showcase custom pipes.
    
    Adds several custom pipes for transforming hero data, including:
    - toggleCase: Toggles the case of a string.
    - canFly: Converts a boolean to a user-friendly "Puede volar" or "No puede volar" string.
    - heroColor: Extracts a string out of the Color enum.
    - heroCreator: Extracts a string out of the Creator enum.
    - heroTextColor: Uses the Color enum to map it to a CSS color hex value.
    - heroFilter: Filters heroes based on a search query.
    - heroSortBy: Sorts heroes based on a specified property.

--- pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,9 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { heroes } from '../../data/heroes.data';
+import { Hero } from '../../interfaces/hero.interface';
+import { CanFyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [
+    ToggleCasePipe,
+    CanFyPipe,
+    HeroColorPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+    HeroCreatorPipe,
+    HeroSortByPipe,
+    HeroFilterPipe,
+  ],
   templateUrl: './custom-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  name = signal('Victor Ramirez');
+
+  upperCase = signal(true);
+
+  heroes = signal(heroes);
+
+  sortBy = signal<keyof Hero | null>(null);
+
+  searchQuery = signal('');
+}
