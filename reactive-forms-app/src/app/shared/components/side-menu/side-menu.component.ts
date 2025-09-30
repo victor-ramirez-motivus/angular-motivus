c7c5170e7f5e4db61fa0e30b4803389c22d91d4f
VICTHOR <ing.victhor1@gmail.com>
2025-09-30 16:00:24 -0600

    Adds a side menu component
    This commit introduces a side menu component to improve navigation.
    
    The side menu displays links to different sections of the application, such as reactive forms, authentication, and country selection.
    It enhances user experience by providing a clear and accessible navigation structure.

--- reactive-forms-app/src/app/shared/components/side-menu/side-menu.component.ts
+++ reactive-forms-app/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { reactiveRoutes } from '../../../reactive/auth.routes';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+    }));
+
+  authMenu: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
+
+  countryMenu: MenuItem[] = [
+    {
+      title: 'Países',
+      route: './country',
+    },
+  ];
+
+}
