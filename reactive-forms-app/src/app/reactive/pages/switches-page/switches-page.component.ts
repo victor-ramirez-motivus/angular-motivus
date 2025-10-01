2074e9e215b6ad48a970455a7986068d798f2507
VICTHOR <ing.victhor1@gmail.com>
2025-09-30 18:34:32 -0600

    Implements reactive form with switches
    Adds a reactive form with gender radio buttons, notification and terms and conditions switches.
    
    Implements form validation for required fields.
    Displays the form value and validation status for debugging.

--- reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    gender: [, Validators.required],
+    wantNotifications: [true],
+    termAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+
+    console.log(this.myForm.value);
+  }
+}
