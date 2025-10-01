f3e19d4e014aadb2cda52d5adfc50f3bdd7f40d4
VICTHOR <ing.victhor1@gmail.com>
2025-10-01 13:41:20 -0600

    Implements cascading country selectors
    Adds reactive form with cascading selectors for region, country, and border.
    Fetches countries based on selected region and borders based on selected country.
    Improves user experience by providing a dynamic and interactive form.

--- reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,75 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { filter, switchMap, tap } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChanged = effect((onCleanup) => {
+    const regionSubscription = this.onRegionChanged();
+    const countrySubscription = this.onCountryChanged();
+
+    onCleanup(() => {
+      regionSubscription.unsubscribe();
+      countrySubscription.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.borders.set([]);
+          this.countriesByRegion.set([]);
+        }),
+        switchMap((region) =>
+          this.countryService.getCountriesByRegion(region ?? '')
+        )
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
+
+  onCountryChanged() {
+    return this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value) => value!.length > 0),
+        switchMap((alphaCode) =>
+          this.countryService.getCountryByAlphaCode(alphaCode ?? '')
+        ),
+        switchMap((country) =>
+          this.countryService.getCountryNamesByCodeArray(country.borders)
+        )
+      )
+
+      .subscribe((borders) => {
+        this.borders.set(borders);
+      });
+  }
+}
