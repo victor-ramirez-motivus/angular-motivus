ec8f32e25266e9b8f8383502b07a6ed81cd5a9bd
VICTHOR <ing.victhor1@gmail.com>
2025-10-02 17:58:05 -0600

    Implements gender-based product filtering
    Adds a page to display products filtered by gender.
    
    This page fetches products based on the gender parameter passed in the route and displays them using the product card component. It uses `rxResource` to handle the asynchronous loading of products.

--- teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/product.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        gender: request.gender,
+      });
+    },
+  });
+}
