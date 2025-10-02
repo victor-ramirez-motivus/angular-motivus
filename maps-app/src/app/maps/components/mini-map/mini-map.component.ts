cabca3b91724778b58d4925205150858d1979ad2
VICTHOR <ing.victhor1@gmail.com>
2025-10-01 19:09:41 -0600

    Adds houses page with mini-map component
    Introduces a houses page displaying a list of properties.
    
    Implements a reusable mini-map component to show the location
    of each house.
    
    Uses dummy data to display house information, including name,
    description, tags and location.

---
+++ maps-app/src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,42 @@
+import { Component, ElementRef, input, viewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
+
+@Component({
+  selector: 'app-mini-map',
+  imports: [],
+  templateUrl: './mini-map.component.html',
+  styles: `
+    div {
+      width: 100%;
+      height: 260px;
+    }
+
+  `,
+})
+export class MiniMapComponent {
+  divElement = viewChild<ElementRef>('map');
+  lngLat = input.required<{ lng: number; lat: number }>();
+  zoom = input<number>(14);
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()!.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat(),
+      zoom: this.zoom(),
+      interactive: false,
+      pitch: 30,
+    });
+
+    new mapboxgl.Marker().setLngLat(this.lngLat()).addTo(map);
+  }
+}
