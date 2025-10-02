eabcbeb17f5bc503b82142744b99125e052fe52c
VICTHOR <ing.victhor1@gmail.com>
2025-10-02 17:55:00 -0600

    home + back

---
+++ teslo-shop/src/app/products/components/product-card/product-card.component.ts
@@ -0,0 +1,21 @@
+import { SlicePipe } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+
+
+@Component({
+  selector: 'app-product-card',
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
+  templateUrl: './product-card.component.html',
+})
+export class ProductCardComponent {
+  product = input.required<Product>();
+
+  imageUrl = computed(() => {
+    return `http://localhost:3000/api/files/product/${
+      this.product().images[0]
+    }`;
+  });
+}
--- teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCardComponent } from "@products/components/product-card/product-card.component";
+import { ProductsService } from '@products/services/product.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({});
+    },
+  });
+}
