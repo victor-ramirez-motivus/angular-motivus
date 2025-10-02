58d3a2d9506c72a4dfd57f56ac1167ec019b454f
VICTHOR <ing.victhor1@gmail.com>
2025-10-01 18:13:34 -0600

    Adds Mapbox key configuration
    Configures the application to use environment variables for the Mapbox API key.
    This improves security by preventing the key from being hardcoded in the source code.
    Also adds a script to set environment variables and updates the .gitignore file to prevent environment files from being committed.
    Updates the build configuration to use environment-specific files.

---
+++ maps-app/scripts/set-envs.js
@@ -0,0 +1,28 @@
+
+const { writeFileSync, mkdirSync } = require( 'fs' );
+
+require( 'dotenv' ).config();
+
+const targetPath = './src/environments/environment.ts';
+const targetPathDev = './src/environments/environment.development.ts';
+
+const mapboxKey = process.env[ 'MAPBOX_KEY' ];
+
+if ( !mapboxKey ) {
+    throw new Error( 'MAPBOX_KEY is not set' );
+}
+
+const envFileContent = `
+export const environment = {
+  mapboxKey: "${ mapboxKey }"
+};
+`;
+
+
+mkdirSync( './src/environments', { recursive: true } );
+
+writeFileSync( targetPath, envFileContent );
+writeFileSync(targetPathDev, envFileContent);
+
+//ejecutar en la terminal
+// node ./scripts/set-envs.js
