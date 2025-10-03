75b905a511480edb0b5ca73cfdda1403c46f3ba6
VICTHOR <ing.victhor1@gmail.com>
2025-10-02 18:08:31 -0600

    Removes unused ProductCardComponent import
    Simplifies the ProductPage component by removing the unused import of `ProductCardComponent` from the component's `imports` array.
    
    Also, simplifies accessing the product images array, removing the optional chaining operator due to the `productResource` now guaranteeing a value.

--- teslo-shop/src/app/store-front/pages/product-page/product-page.component.ts
+++ teslo-shop/src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
-import { ProductCardComponent } from "@products/components/product-card/product-card.component";
 import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
 import { ProductsService } from '@products/services/product.service';
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductCardComponent, ProductCarouselComponent],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
